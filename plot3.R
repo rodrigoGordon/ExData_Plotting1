e771420180fa9e3ee2720482b5f4037def5ee3fd
Rodrigo <rodrigo@Rodrigos-Air.home>
2015-12-11 01:13:26 -0200

    Adding images and code for plots 3 and 4

--- plot3.R
+++ plot3.R
@@ -5,7 +5,6 @@ householdDataSet <- read.csv("household_power_consumption.txt",
 )
 
 householdDataSet$Date <- as.Date(householdDataSet$Date, "%d/%m/%Y")
- 
 
 #credits to http://stackoverflow.com/questions/11464578/r-subsetting-a-data-frame-using-a-list-of-dates-as-the-filter
 dateSubSet <- householdDataSet[
@@ -16,14 +15,18 @@ dateSubSet <- householdDataSet[
 dateSubSet <- transform(dateSubSet, 
                         timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
 
- 
+
 plot(dateSubSet$timestamp,
-     dateSubSet$Global_active_power,
+     dateSubSet$Sub_metering_1,
      type = "l", 
      xlab = "",
-     ylab = "Global Active Power (kilowatts)"
-     )
+     ylab = "Energy sub metering"
+)
 
-png("plot2.png", width = 480, height = 480, res = 72)
+lines(dateSubSet$timestamp,dateSubSet$Sub_metering_2, col="red")
+lines(dateSubSet$timestamp,dateSubSet$Sub_metering_3, col="blue")
 
+legend(x = "topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = 1,col=c("black","red","blue"))
 
+png("plot3.png", width = 480, height = 480, res = 72)
--- plot4.R
+++ plot4.R
@@ -5,7 +5,6 @@ householdDataSet <- read.csv("household_power_consumption.txt",
 )
 
 householdDataSet$Date <- as.Date(householdDataSet$Date, "%d/%m/%Y")
- 
 
 #credits to http://stackoverflow.com/questions/11464578/r-subsetting-a-data-frame-using-a-list-of-dates-as-the-filter
 dateSubSet <- householdDataSet[
@@ -16,14 +15,42 @@ dateSubSet <- householdDataSet[
 dateSubSet <- transform(dateSubSet, 
                         timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
 
- 
+par(mfrow=c(2,2))
+
 plot(dateSubSet$timestamp,
      dateSubSet$Global_active_power,
      type = "l", 
      xlab = "",
      ylab = "Global Active Power (kilowatts)"
-     )
+)
 
-png("plot2.png", width = 480, height = 480, res = 72)
+plot(dateSubSet$timestamp,
+     dateSubSet$Voltage,
+     type = "l", 
+     xlab = "datetime",
+     ylab = "Voltage"
+)
+
+plot(dateSubSet$timestamp,
+     dateSubSet$Sub_metering_1,
+     type = "l", 
+     xlab = "",
+     ylab = "Energy sub metering"
+)
+
+lines(dateSubSet$timestamp,dateSubSet$Sub_metering_2, col="red")
+lines(dateSubSet$timestamp,dateSubSet$Sub_metering_3, col="blue")
+
+legend(x = "topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = 1,col=c("black","red","blue"))
 
 
+plot(dateSubSet$timestamp,
+     dateSubSet$Global_reactive_power,
+     type = "l", 
+     xlab = "",
+     ylab = "Global Reactive Power (kilowatts)"
+)
+
+
+png("plot4.png", width = 480, height = 480, res = 72)
